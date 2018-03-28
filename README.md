18f0ad5c4aeff674353bc7726f33dcfa53da7464
Reisclef <9120292+Reisclef@users.noreply.github.com>
2018-03-28 22:13:22 +0100

    Limited search to body, added Markdown file

---
+++ README.md
@@ -0,0 +1,23 @@
+#BrowserTextReplace
+
+For anyone who wants a lighter kind of newsreading
+
+##Origins
+
+I was originally inspired to create this by Last Week Tonight's [Drumpfinator](https://chrome.google.com/webstore/detail/drumpfinator/hcimhbfpiofdihhdnofbdlhjcmjopilp)  
+
+The main problems I had with it were that:
+
+- I had no idea what code was running behind it
+- It only replaced certain words
+
+Therefore, I decided to create a simple Chrome Extension to allow a user to find and replace whatever words they might want. Furthermore, I decided to open-source the project in order to keep it transparent so anyone could see the code.
+
+##Example
+
+For example if you replace "Brexit" with "Breakfast" (as a few politicians have inadvertantly done), you get gems such as:
+
+[Breakfast could leave Britain with a bare larder, farmers warn](https://www.theguardian.com/environment/2017/aug/05/brexit-could-leave-britain-with-a-bare-larder-farmers-warn)  
+
+
+
--- replace.js
+++ replace.js
@@ -1,31 +1,37 @@
 //Grab the settings from Chrome Storage
 chrome.storage.sync.get("textReplaceSettings", function(settings){
-    replaceText(settings.textReplaceSettings);
+    if (settings.textReplaceSettings){
+        replaceText(settings.textReplaceSettings);
+    }
 })
 
 //Test if the node is Text, and then find and replace all instances from settings 
 function replaceText(settings){
 
-    var elements = document.getElementsByTagName('*');
+    var elements = document.getElementsByTagName('BODY')[0].getElementsByTagName('*');
+    
+    //Blacklist certain element tags to avoid breaking the page
+    var elementsToSkip = ["META", "SCRIPT", "LINK", "STYLE", "NOSCRIPT"]
 
     for (var thisElement = 0; thisElement < elements.length; thisElement++) {
         var element = elements[thisElement];
-        for (var thisNode = 0; thisNode < element.childNodes.length; thisNode++) {
-            var node = element.childNodes[thisNode];
-            console.log(node)
-            if (node.nodeType === 3) {
-                var text = node.nodeValue;
-                var replacedText;
-                for (var i = 0; i < settings.length; i++){
-                    if (i == 0){
-                        replacedText = text.replace(new RegExp(settings[i][0],"gi"),settings[i][1])
+        if (elementsToSkip.indexOf(element.tagName) == -1){
+            for (var thisNode = 0; thisNode < element.childNodes.length; thisNode++) {
+                var node = element.childNodes[thisNode];
+                if (node.nodeType === 3) {
+                    var text = node.nodeValue;
+                    var replacedText;
+                    for (var i = 0; i < settings.length; i++) {
+                        if (i == 0) {
+                            replacedText = text.replace(new RegExp(settings[i][0], "gi"), settings[i][1])
+                        }
+                        else {
+                            replacedText = replacedText.replace(new RegExp(settings[i][0], "gi"), settings[i][1])
+                        }
                     }
-                    else {
-                        replacedText = replacedText.replace(new RegExp(settings[i][0],"gi"),settings[i][1])
+                    if (replacedText !== text) {
+                        element.replaceChild(document.createTextNode(replacedText), node);
                     }
-                }
-                if (replacedText !== text) {
-                    element.replaceChild(document.createTextNode(replacedText),node);
                 }
             }
         }
